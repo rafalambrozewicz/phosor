18cdfd2537f2db10d58ffc72744043e55d739a9c
Rafal Ambrozewicz <rafal.ambrozewicz@gmail.com>
2019-12-01 14:43:56 +0100

    Added `shadow` gradle plugin for creating fat jars

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.jetbrains.kotlin.jvm").version("1.3.21")
     application
+    id("org.jetbrains.kotlin.jvm").version("1.3.21")
+    id("com.github.johnrengelman.shadow") version "5.2.0"
 }
 
 repositories {
